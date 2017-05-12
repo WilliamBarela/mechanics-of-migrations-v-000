c30f44da7672976bb75b799906992203c87849d3
WilliamBarela <william.barela.dev@gmail.com>
2017-05-12 13:54:51 +0900

    forgot to fork. copied work from previous clone

--- Rakefile
+++ Rakefile
@@ -3,6 +3,9 @@ require 'sinatra/activerecord/rake'
 
 task :console do
   require 'irb'
+  #require 'pry'
   ARGV.clear
+  #pry.binding
+  #system('sqlite3 ./db/songs.db "SELECT * FROM artists"')
   IRB.start
 end
--- artist.rb
+++ artist.rb
@@ -2,6 +2,13 @@ class Artist < ActiveRecord::Base
 
   def self.create_table
     # create table using SQL here
+    ActiveRecord::Base.connection.execute(<<-SQL)
+        CREATE TABLE IF NOT EXISTS artists (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        length INTEGER
+        );
+    SQL
   end
 
 end
